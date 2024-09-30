c7a1ee862b71d2f59dbb4ab2c3c509bb52c3c541
yudong <koreacio@gmail.com>
2024-09-30 09:22:16 +0900

    [chap11] Fix std::formatter for bw::numword

--- chap11/numword/numword-test.cpp
+++ chap11/numword/numword-test.cpp
@@ -5,6 +5,14 @@
 
 #include "numword.h"
 
+template<>
+struct std::formatter<bw::numword>: std::formatter<unsigned> {
+    auto format(const bw::numword& nw, std::format_context& ctx) const {
+        bw::numword _nw{nw}; 
+        return format_to(ctx.out(), "{}", _nw.words());
+    }
+};
+
 namespace bw {
     template<typename... Args>
     constexpr void print(const std::string_view str_fmt, Args&&... args) {
@@ -16,18 +24,18 @@ int main() {
     bw::numword nw{};
     uint64_t n{};
 
-    bw::print("n is {}, {}\n", nw.getnum(), nw.words());
+    bw::print("n is {}, {}\n", nw.getnum(), nw);
 
-    nw = 3; bw::print("n is {}, {}\n", nw.getnum(), nw.words());
-    nw = 47; bw::print("n is {}, {}\n", nw.getnum(), nw.words());
-    nw = 73; bw::print("n is {}, {}\n", nw.getnum(), nw.words());
-    nw = 1492; bw::print("n is {}, {}\n", nw.getnum(), nw.words());
-    nw = 10012; bw::print("n is {}, {}\n", nw.getnum(), nw.words());
+    nw = 3; bw::print("n is {}, {}\n", nw.getnum(), nw);
+    nw = 47; bw::print("n is {}, {}\n", nw.getnum(), nw);
+    nw = 73; bw::print("n is {}, {}\n", nw.getnum(), nw);
+    nw = 1492; bw::print("n is {}, {}\n", nw.getnum(), nw);
+    nw = 10012; bw::print("n is {}, {}\n", nw.getnum(), nw);
     
-    n = 100073; bw::print("n is {}, {}\n", n, bw::numword{n}.words());
-    n = 1000000; bw::print("n is {}, {}\n", n, bw::numword{n}.words());
-    n = 1000000001; bw::print("n is {}, {}\n", n, bw::numword{n}.words());
-    n = 123000000000; bw::print("n is {}, {}\n", n, bw::numword{n}.words());
+    n = 100073; bw::print("n is {}, {}\n", n, bw::numword{n});
+    n = 1000000; bw::print("n is {}, {}\n", n, bw::numword{n});
+    n = 1000000001; bw::print("n is {}, {}\n", n, bw::numword{n});
+    n = 123000000000; bw::print("n is {}, {}\n", n, bw::numword{n});
 
     n = 474142398123; bw::print("n is {}, {}\n", n, nw(n));
     n = 1474142398007; bw::print("n is {}, {}\n", n, nw(n));
